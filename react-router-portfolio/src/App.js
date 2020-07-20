733c9fd7011761fd0f0a3a5923c02a9165271029
Milcah <milcah@macbook-air.lan>
2020-07-20 09:48:32 -0700

    import react router dom router, switch, route, link and create navigation

--- react-router-portfolio/src/App.js
+++ react-router-portfolio/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom'
 import Homepage from './pages/Homepage'
 import About from './pages/About'
 import Blog from './pages/Blog'
@@ -11,10 +12,52 @@ function App() {
       <header className="App-header">
         <h1>React Router DOM</h1>
       </header>
-      <Homepage />
-      <About />
-      <Blog />
-      <Projects />
+      <nav>
+        <ul>
+          <Link to='/'>
+            <li>
+              Homepage
+            </li>
+          </Link>
+          <Link to='/about'>
+            <li>
+              About
+            </li>
+          </Link>
+          <Link to='/blog'>
+            <li>
+              Blog
+            </li>
+          </Link>
+          <Link to='/projects'>
+            <li>
+              Projects
+            </li>
+          </Link>
+        </ul>
+      </nav>
+      <Switch>
+        <Route exact path='/' render={() =>
+          <>
+            <Homepage />
+          </>
+        } />
+        <Route exact path='/about' render={() =>
+          <>
+            <About />
+          </>
+        } />
+        <Route exact path='/blog' render={() =>
+          <>
+            <Blog />
+          </>
+        } />
+        <Route exact path='/projects' render={() =>
+          <>
+            <Projects />
+          </>
+        } />
+      </Switch>
     </div>
   );
 }
