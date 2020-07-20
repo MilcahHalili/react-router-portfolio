1f4f159c69c7bc2ee5f7be0c0096d5b7f91b9cf3
Milcah <milcah@macbook-air.lan>
2020-07-20 10:50:30 -0700

    fix key warning ffor blog posts

--- react-router-portfolio/src/pages/Blog.js
+++ react-router-portfolio/src/pages/Blog.js
@@ -4,8 +4,8 @@ const Blog = (props) => {
   return (
     <div>
       <h2>Blog</h2>
-      {props.posts.map(post =>
-        <div>
+      {props.posts.map((post, i) =>
+        <div key={i} >
           <h3>{post.title}</h3>
           <p>{post.body}</p>
         </div>
