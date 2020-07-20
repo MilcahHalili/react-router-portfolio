525cecafc784e74a2510b800ed97656ad5eae60a
Milcah <milcah@macbook-air.lan>
2020-07-20 09:35:59 -0700

    create functional components

--- react-router-portfolio/src/App.js
+++ react-router-portfolio/src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import Homepage from './pages/Homepage'
+import About from './pages/About'
+import Blog from './pages/Blog'
+import Projects from './pages/Projects'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React Router DOM</h1>
       </header>
+      <Homepage />
+      <About />
+      <Blog />
+      <Projects />
     </div>
   );
 }
